1c69cafb9df623292e75f345e26e82f3ff64a4b0
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-04-20 19:28:12 +0300

    webpack progress...

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,43 @@
+function tabs() {
+    // tabs
+
+    const tabsParents = document.querySelector('.tabheader__items'),
+    tabs = tabsParents.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.remove('show', 'fade');
+            item.classList.add('hide');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParents.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+}
+
+module.exports = tabs;
