39c17ac4d827d3413b501a7dc08caca50a131f51
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-11-12 20:13:35 +0300

    some commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,15 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/js/script.js',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/src/js'
+    path: __dirname + '/dist/js'
   },
   watch: true,
 
   devtool: "source-map",
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/, // мы находим наши js файлы (регулярное выражение)
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', {
-                debug: true,
-                // corejs: 3,
-                useBuiltIns: "usage"
-            }]]
-          }
-        }
-      }
-    ]
-  }
+  module: {}
 };
